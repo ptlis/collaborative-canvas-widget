bd511bce5ff0089421b92b4ad536871cd777ed67
ptlis <ptlis@ptlis.net>
2012-11-30 22:15:48 +0000

    Partial integration of requirejs.

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+/*jshint jquery:true */
+
+
+require([
+
+            'canvasStorage',
+            'cards',
+            'connections',
+            'containers',
+            'decks',
+            'fields',
+            'util',
+
+            'card_specific/templates/selectable_prompts',
+            'card_specific/multimedia',
+            'card_specific/sticky',
+            'card_specific/ld',
+            'card_specific/hlm',
+            'card_specific/information_skills',
+            'card_specific/assessment'
+        ],
+        function($) {
+
+
+
+
+});
