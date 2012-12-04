b3d65011004f90c2a12a4fa0f0d243d06f2d829c
Brian Ridley <brian@ubuntu.(none)>
2012-12-04 12:44:30 +0000

    Refactor of javascript to use RequireJS for dependancy handling & module loading. Added ROADMAP.

---
+++ src/requirejs/main.js
@@ -0,0 +1,72 @@
+/*jshint jquery:true */
+
+
+requirejs.config({
+    shim: {
+        'jquery.base64': {
+            'deps':	    ['jquery'],
+            'exports':  'jquery.fn.base64'
+        },
+
+        'jquery.contextMenu': {
+            'deps':     ['jquery'],
+            'exports':  'jquery.fn.contextMenu'
+        },
+        
+        'jquery.event.drag': {
+            'deps':     ['jquery'],
+            'exports':  'jquery.fn.drag'
+        },
+        
+        'jquery.event.drag.live': {
+            'deps':     ['jquery', 'jquery.event.drag']
+        },
+        
+        'jquery.event.drop': {
+            'deps':     ['jquery'],
+            'exports':  'jquery.fn.drop'
+        },
+        
+        'jquery.event.drop.live': {
+            'deps':     ['jquery']
+        },
+        
+        'jquery.ui.position': {
+            'deps':     ['jquery']
+        }
+    }
+});
+
+require([
+
+            'lib/jquery',
+            'lib/jquery.base64',
+            'lib/jquery.contextMenu',
+            'lib/jquery.event.drag',
+            'lib/jquery.event.drag.live',
+            'lib/jquery.event.drop',
+            'lib/jquery.event.drop.live',
+            'lib/jquery.ui.position',
+
+            'canvasStorage',
+            'cards',
+            'connections',
+            'containers',
+            'decks',
+            'fields',
+            'util',
+
+            'card_specific/templates/selectable_prompts',
+            'card_specific/multimedia',
+            'card_specific/sticky',
+            'card_specific/ld',
+            'card_specific/hlm',
+            'card_specific/information_skills',
+            'card_specific/assessment'
+        ],
+        function($) {
+
+
+
+
+});
