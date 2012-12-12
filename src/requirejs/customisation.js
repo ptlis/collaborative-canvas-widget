b65c8a5864a6b56bb09d22ac31c9432b7fbf4844
ptlis <ptlis@ptlis.net>
2012-12-12 00:03:18 +0000

    Further work integrating card & deck customisations - skeleteton of dialog for deck customisation options.

--- src/requirejs/customisation.js
+++ src/requirejs/customisation.js
@@ -1,8 +1,8 @@
 /*jshint jquery:true */
 
 
-define( ['jquery'],
-        function($) {
+define( ['jquery', 'util'],
+        function($, util) {
             'use strict';
             
             var customisation   = {};
@@ -21,18 +21,18 @@ define( ['jquery'],
             customisation.dialogs.deckSelection = function() {
                 
                 // Background & dialog
-                var bgElem =            $('<div></div>',  {
+                var bgElem          =   $('<div></div>',  {
                                             'class':       'dialog_background'
                                         });
                 bgElem.css('display', 'none');
                 bgElem.appendTo($('body'));
 
-                var dialogCont =        $('<div></div>', {
+                var dialogCont      =   $('<div></div>', {
                                             'class':       'dialog_container'
                                         });
                 dialogCont.appendTo(bgElem);
                 
-                var dialog =            $('<div></div>', {
+                var dialog          =   $('<div></div>', {
                                             'class':    'dialog',
                                             'id':       'customisation_deck_selection'
                                         });
@@ -41,8 +41,8 @@ define( ['jquery'],
                 
                 // Title & Dismiss button
                 var title           =   $('<div></div>', {
-                    'class':    'dialog_title'
-                });
+                                            'class':    'dialog_title'
+                                        });
                 title.text('Customisation');
                 title.appendTo(dialog);
 
@@ -72,6 +72,9 @@ define( ['jquery'],
                                         });
                 newDeck.text('Create New Deck');
                 newDeck.appendTo(options);
+                newDeck.on('click', function() {
+                    customisation.dialogs.deckCustomisation(true);
+                });
                 
                 var editDeck =          $('<li></li>', {
                                             'id':   'edit_deck'
@@ -91,6 +94,100 @@ define( ['jquery'],
                     function() {
                     });
             };
+            
+
+            customisation.dialogs.deckCustomisation = function(newDeck, deckName) {
+
+                var dialog          =   $('<div></div>', {
+                                            'class':    'dialog',
+                                            'id':       'customisation_deck_details'
+                                        });
+                dialog.css('display', 'none');
+                
+                // Title & Dismiss button
+                var title           =   $('<div></div>', {
+                                            'class':    'dialog_title'
+                                        });
+                if(newDeck) {
+                    title.text('Enter New Deck Details');
+                }
+                else {
+                    title.text('Customise Deck Details');
+                }
+                title.appendTo(dialog);
+
+                var dismissButton   =   $('<div></div>', {
+                                            'class':    'dialog_dismiss dismiss_button_32x32'
+                                        });
+                dismissButton.appendTo(dialog);
+                
+                var bgElem          = $('.dialog_background');
+                dismissButton
+                    .off('click')
+                    .on('click', function() {
+                        bgElem.fadeOut( 250,
+                            function() {
+                                bgElem.remove();
+                            });
+                    });
+                
+                
+                var content     =   $('<ul></ul>', {
+                                        'id':       'deck_customisations'
+                                    });
+                content.appendTo(dialog);
+                
+                
+                // Deck Title & Description
+                var titleCont   =   $('<li></li>', {
+                                        'class':    'title_container'
+                                    });
+                titleCont.appendTo(content);
+                
+                var titleLabel  =   $('<label></label>', {
+                                        'for':      'title'
+                                    });
+                titleLabel.text('Name:');
+                titleLabel.appendTo(titleCont);
+                
+                var titleEntry  =   $('<input>', {
+                                        'type':     'text',
+                                        'id':       'title'
+                                    });
+                titleEntry.appendTo(titleCont);
+
+
+                var descCont    =   $('<li></li>', {
+                                        'class':    'desc_container'
+                                    });
+                descCont.appendTo(content);
+                
+                var descLabel   =   $('<label></label>', {
+                                        'for':      'desc'
+                                    });
+                descLabel.text('Description:');
+                descLabel.appendTo(descCont);
+                
+                var descEntry  =   $('<input>', {
+                                        'type':     'text',
+                                        'id':       'desc'
+                                    });
+                descEntry.appendTo(descCont);
+                
+                
+                // Transition between dialogs
+                var oldDialog  = $('.dialog');
+                
+                oldDialog.fadeOut(  250,
+                    function() {
+                        oldDialog.remove();
+                        dialog.appendTo('.dialog_container');
+                        dialog.fadeIn( 250);
+                    });
+                
+            };
+            
+            
             
             customisation.init();
             
